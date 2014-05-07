222e67190bbbb37c415d7b9647d0784ba43db847
Team Diamond <cryolt2@gmail.com>
2014-05-07 19:15:18 +0200

    LT1 scripts

--- scripts/lt1_scripts/setup/sequence.py
+++ scripts/lt1_scripts/setup/sequence.py
@@ -48,5 +48,24 @@ qt.pulsar.set_channel_opt('YellowAOM','low', qt.instruments['YellowAOM'].get_sec
 #    high=0.4, low=0, offset=0., delay=750e-9, active=True)
 
 # Trigger AWG LT2
-qt.pulsar.define_channel(id='ch3_marker1', name='scope_sync', type='marker', 
-    high=2.0, low=0, offset=0., delay=0., active=True)
+qt.pulsar.define_channel(id='ch3_marker1', name='scope_pulse', type='marker', 
+    high=0.5, low=0, offset=0., delay=0., active=True)
+
+qt.pulsar.AWG_sequence_cfg={
+        'SAMPLING_RATE'             :   1e9,
+        'CLOCK_SOURCE'              :   1,    # Internal | External
+        'REFERENCE_SOURCE'          :   2,    # Internal | External
+        'EXTERNAL_REFERENCE_TYPE'   :   1,    # Fixed | Variable
+        'REFERENCE_CLOCK_FREQUENCY_SELECTION':1, #10 MHz | 20 MHz | 100 MHz
+        'TRIGGER_SOURCE'            :   1,    # External | Internal
+        'TRIGGER_INPUT_IMPEDANCE'   :   1,    # 50 ohm | 1 kohm
+        'TRIGGER_INPUT_SLOPE'       :   1,    # Positive | Negative
+        'TRIGGER_INPUT_POLARITY'    :   1,    # Positive | Negative
+        'TRIGGER_INPUT_THRESHOLD'   :   1.4,  # V
+        'EVENT_INPUT_IMPEDANCE'     :   2,    # 50 ohm | 1 kohm
+        'EVENT_INPUT_POLARITY'      :   1,    # Positive | Negative
+        'EVENT_INPUT_THRESHOLD'     :   1.4,  #V
+        'JUMP_TIMING'               :   1,    # Sync | Async
+        'RUN_MODE'                  :   4,    # Continuous | Triggered | Gated | Sequence
+        'RUN_STATE'                 :   0,    # On | Off
+}
