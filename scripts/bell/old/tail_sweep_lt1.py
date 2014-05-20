4e0e34d69d0c79590597a7e9a205dbf45484b2cd
Team Diamond <cryolt2@gmail.com>
2014-05-20 10:21:40 +0200

    unified tail_sweep.py

--- scripts/bell/old/tail_sweep_lt1.py
+++ scripts/bell/old/tail_sweep_lt1.py
@@ -1,5 +1,6 @@
 """
-LT3 script for Measuring a tail with a picoquant time correlator
+LT1 script for Measuring a tail with a picoquant time correlator
+is this identical to the lt3 version??
 """
 
 
@@ -19,29 +20,31 @@ import sequence as bseq
 reload(bseq)
 import params
 reload(params)
+import params_lt1
+reload(params_lt1)
 
-class LT3Tail(bell.Bell):
+class LT1Tail(bell.Bell):
     adwin_process = pulsar_pq.PQPulsarMeasurement.adwin_process
     
     def generate_sequence(self):
   
-        self.lt3_seq = pulsar.Sequence('TailLT3')
+        self.lt1_seq = pulsar.Sequence('TailLT1')
 
         elements = [] 
 
         for i in range(self.params['pts']):
             eom_p = self.create_eom_pulse(i)
             e = bseq._LDE_element(self, 
-                name = 'LT3 Tail sweep element {}'.format(i),
+                name = 'LT1 Tail sweep element {}'.format(i),
                eom_pulse =  eom_p)    
             elements.append(e)
-            self.lt3_seq.append(name = 'LT3 Tail sweep {}'.format(i),
+            self.lt1_seq.append(name = 'LT1 Tail sweep {}'.format(i),
                 wfname = e.name,
                 trigger_wait = self.params['trigger_wait'],
                 repetitions = self.params['LDE_attempts_before_CR'])
             
         qt.pulsar.upload(*elements)
-        qt.pulsar.program_sequence(self.lt3_seq)
+        qt.pulsar.program_sequence(self.lt1_seq)
 
     def save(self):
         pulsar_pq.PQPulsarMeasurement.save(self)
@@ -54,11 +57,11 @@ class LT3Tail(bell.Bell):
                     aom_channel = 'EOM_AOM_Matisse',
                     eom_off_duration = self.params['eom_off_duration'],
                     eom_off_amplitude = self.params['eom_off_amplitude'],
-                    eom_off_2_amplitude  = 2.65, #msmt.params_lt3['eom_off_2_amplitude'],
+                    eom_off_2_amplitude  = 2.65, #msmt.params_lt1['eom_off_2_amplitude'],
                     eom_overshoot_duration1 = self.params['eom_overshoot_duration1'],
-                    eom_overshoot1 = 0.0, #msmt.params_lt3['eom_overshoot1'],
+                    eom_overshoot1 = 0.0, #msmt.params_lt1['eom_overshoot1'],
                     eom_overshoot_duration2 = self.params['eom_overshoot_duration2'],
-                    eom_overshoot2 = 0.0, #msmt.params_lt3['eom_overshoot2'],
+                    eom_overshoot2 = 0.0, #msmt.params_lt1['eom_overshoot2'],
                     aom_risetime = self.params['aom_risetime']) 
         elif self.params['use_eom_pulse'] == 'raymond-pulse':
             return eom_pulses.EOMAOMPulse_raymond_pulse('Eom Aom Pulse', 
@@ -100,20 +103,20 @@ class LT3Tail(bell.Bell):
                     aom_amplitude           = self.params['aom_amplitude'][i])
 
 
-def tail_lt3(name):
+def tail_lt1(name):
 
-    m=LT3Tail(name)
+    m=Tail(name)
 
     m.params.from_dict(qt.exp_params['protocols']['AdwinSSRO'])
 
     m.joint_params = {}
-    for k in params.params_lt3:
-        m.params[k] = params.params_lt3[k]
+    for k in params_lt1.params_lt1:
+        m.params[k] = params_lt1.params_lt1[k]
     for k in params.joint_params:
         m.params[k] = params.joint_params[k]
         m.joint_params[k] = params.joint_params[k]
 
-    pts=11
+    pts=10
     m.params['pts']=pts
     
     #EOM pulse ----------------------------------
@@ -121,8 +124,8 @@ def tail_lt3(name):
     #qt.pulsar.set_channel_opt('EOM_trigger', 'high', 2.)#2.0
 
     m.params['use_eom_pulse'] = 'normal'#raymond-step' #'short', 'raymond-pulse', 'raymond-step'
-    m.params['eom_off_amplitude']         = np.ones(pts)*-0.07#np.linspace(-0.1,0.05,pts) # calibration from 19-03-2014
-    m.params['aom_risetime']              = 25e-9 # calibration to be done!
+    m.params['eom_off_amplitude']         = np.ones(pts)*-0.26#np.linspace(-0.1,0.05,pts) # calibration from 19-03-2014
+    m.params['aom_risetime']              = 25e-9#42e-9 # calibration to be done!
    
     if m.params['use_eom_pulse'] == 'raymond-pulse':
 
@@ -142,8 +145,8 @@ def tail_lt3(name):
                                                     +m.params['eom_pulse_duration']*m.params['eom_off_amplitude'] )/(2.*m.params['eom_pulse_duration'])  
     else:#'normal':
 
-        m.params['eom_pulse_amplitude']        = np.ones(pts)*2.0 #(for long pulses it is 1.45, dor short:2.0)calibration from 19-03-2014# 
-        m.params['eom_pulse_duration']         = np.ones(pts)*2e-9
+        m.params['eom_pulse_amplitude']        = np.ones(pts)*1.5 #(for long pulses it is 1.45, dor short:2.0)calibration from 19-03-2014# 
+        m.params['eom_pulse_duration']         = np.ones(pts)* 2e-9
         m.params['eom_comp_pulse_amplitude']   = m.params['eom_pulse_amplitude'] 
         m.params['eom_off_duration']           = 200e-9
         m.params['eom_overshoot_duration1']    = 10e-9
@@ -159,18 +162,19 @@ def tail_lt3(name):
     for i,p in enumerate(aom_power_sweep):
         aom_voltage_sweep[i]= p_aom.power_to_voltage(p)
 
-    m.params['aom_amplitude'] = aom_voltage_sweep #np.ones(pts)*1.0#aom_voltage_sweep 
+    m.params['aom_amplitude']             = aom_voltage_sweep#np.ones(pts)*1.0#aom_voltage_sweep#np.ones(pts)*1.0#aom_voltage_sweep 
 
     m.params['sweep_name'] = 'aom_amplitude [percent]'
     m.params['sweep_pts'] = aom_power_sweep/max_power_aom
 
-    bseq.pulse_defs_lt3(m)
-    m.params['MIN_SYNC_BIN'] =       5000 
-    m.params['MAX_SYNC_BIN'] =       7000
+    bseq.pulse_defs_lt1(m)
+    m.params['MIN_SYNC_BIN'] =       6000 
+    m.params['MAX_SYNC_BIN'] =       8000
 
+    m.params['sync_during_LDE'] = 1
     m.params['send_AWG_start'] = 1
     m.params['syncs_per_sweep'] = m.params['LDE_attempts_before_CR']
-    m.params['repetitions'] = 5000
+    m.params['repetitions'] = 10000
     m.params['SP_duration'] = 250
 
     m.joint_params['opt_pi_pulses'] = 1
@@ -178,10 +182,9 @@ def tail_lt3(name):
     m.params['MW_during_LDE'] = 0
     
     debug=False
-    m.params['trigger_wait'] = True#not(debug)
+    m.params['trigger_wait'] = 1
     m.autoconfig()
     m.generate_sequence()
-    #print m.params['measurement_time']
 
     m.setup(debug=debug)
     m.run(autoconfig=False, setup=False,debug=debug)    
@@ -190,4 +193,4 @@ def tail_lt3(name):
 
 
 if __name__ == '__main__':
-    tail_lt3('lt3_tailS_The111no2_SIL1_Ey_+11deg')
+    tail_lt1('lt1_tail_test')
