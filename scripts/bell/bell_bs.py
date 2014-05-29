1ee712ffaaf0c00e304c9f8777d46758b7f96624
Team diamond lt3 <cryolt2@gmail.com>
2014-05-29 10:17:34 +0200

    bell BASH

--- scripts/bell/bell_bs.py
+++ scripts/bell/bell_bs.py
@@ -8,8 +8,8 @@ def remote_HH_measurement(name):
     m=bell.Bell_BS(qt.instruments['remote_measurement_helper'].get_measurement_name())
     m.params['MAX_DATA_LEN']        =   int(100e6)
     m.params['BINSIZE']             =   1  #2**BINSIZE*BASERESOLUTION
-    m.params['MIN_SYNC_BIN']        =   0 
-    m.params['MAX_SYNC_BIN']        =   1000 
+    m.params['MIN_SYNC_BIN']        =   5000 
+    m.params['MAX_SYNC_BIN']        =   7000 
     m.params['measurement_time']    =   1200 #sec
     m.params['measurement_abort_check_interval']    = 1. #sec
     m.run()
