969aba66d3b03e2193cd99153df7582c32d8a210
Teasm Diamond <cryolt2@gmail.com>
2014-06-10 13:56:38 +0200

    bell bash

--- scripts/bell/bell_bs.py
+++ scripts/bell/bell_bs.py
@@ -6,10 +6,10 @@ import measurement.lib.measurement2.pq.pq_measurement as pq
 
 
 bs_params = {}
-bs_params['MAX_DATA_LEN']        =   joint_params['MAX_DATA_LEN']
+bs_params['MAX_DATA_LEN']        =   int(100e6)
 bs_params['BINSIZE']             =   8  #2**BINSIZE*BASERESOLUTION = 1 ps for HH
-bs_params['MIN_SYNC_BIN']        =   5000*4 #5 us 
-bs_params['MAX_SYNC_BIN']        =   7000*4 #7 us
+bs_params['MIN_SYNC_BIN']        =   int(5e-6*1e12) #5 us 
+bs_params['MAX_SYNC_BIN']        =   int(15e-6*1e12) #15 us
 bs_params['measurement_time']    =   24*60*60 #sec = 24H
 bs_params['measurement_abort_check_interval']    = 1 #sec
 
@@ -23,6 +23,7 @@ class Bell_BS(pq.PQMeasurement):
         print remote_params
         for k in remote_params:
             self.params[k] = remote_params[k]
+        self.remote_measurement_helper.set_data_path(self.h5datapath)
 
     def start_measurement_process(self):
         self.remote_measurement_helper.set_is_running(True)
@@ -39,18 +40,17 @@ class Bell_BS(pq.PQMeasurement):
     def finish(self):
         self.save_instrument_settings_file()
         self.save_params()
-        self.remote_measurement_helper.set_data_path(self.h5datapath)
         pq.PQMeasurement.finish(self)
 
 Bell_BS.remote_measurement_helper = qt.instruments['remote_measurement_helper']
 
 def remote_HH_measurement(name):
-
+    debug=False
     m=Bell_BS(name+'_'+qt.instruments['remote_measurement_helper'].get_measurement_name())
     for k in bs_params:
         m.params[k] = bs_params[k]
-    m.run()
+    m.run(debug=debug)
     m.finish()
     
 if __name__ == '__main__':
-    remote_HH_measurement('test_BS')
+    remote_HH_measurement('tpqi_parallel_BS')
