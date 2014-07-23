c531eee46adc03c3421339ff12859881fd827089
team_diamond_lt2 <tim.taminiau@gmail.com>
2014-07-23 14:26:11 +0200

    New scripts, THT

---
+++ scripts/QEC/carbon_ramseys/carbon_ramsey_script_noDD.py
@@ -0,0 +1,74 @@
+"""
+Script for a carbon ramsey sequence, without electron DD during the waiting time.
+"""
+import numpy as np
+import qt
+
+#reload all parameters and modules
+execfile(qt.reload_current_setup)
+import measurement.lib.measurement2.adwin_ssro.dynamicaldecoupling as DD
+import measurement.scripts.mbi.mbi_funcs as funcs
+reload(DD)
+
+SAMPLE = qt.exp_params['samples']['current']
+SAMPLE_CFG = qt.exp_params['protocols']['current']
+
+def Carbon_Ramsey(name, tau = None, N=None):
+
+    m = DD.NuclearRamsey_no_elDD(name)
+
+    funcs.prepare(m)
+
+    '''set experimental parameters'''
+    m.params['reps_per_ROsequence'] = 500 #Repetitions of each data point
+    m.params['Initial_Pulse']       = 'x'
+    m.params['Final_Pulse']         = '-x'
+    m.params['Decoupling_sequence_scheme'] = 'repeating_T_elt'
+
+    m.params['addressed_carbon'] = 4 
+
+    ### Sweep parameter ###
+    m.params['free_evolution_times'] = np.linspace(3.2e3,45e3,41).astype(int)*1e-9
+    
+
+    print 'free evolution times: %s' %m.params['free_evolution_times']
+    m.params['pts']              = len(m.params['free_evolution_times'])
+    m.params['sweep_pts']        = m.params['free_evolution_times']
+    m.params['sweep_name']       = 'Free evolution time'
+
+    if N ==None: 
+        m.params['C_Ren_N'] = m.params['C4_Ren_N'][0]  
+    else:
+        m.params['C_Ren_N'] = N
+    if tau ==None: 
+        m.params['C_Ren_tau'] = m.params['C4_Ren_tau'][0]
+    else: 
+        m.params['C_Ren_tau'] = tau 
+
+
+    #############################
+    #!NB: These should go into msmt params
+    #############################
+    m.params['min_dec_tau'] = 20e-9 + m.params['fast_pi_duration']/2.0
+    m.params['max_dec_tau'] = 0.35e-6 #Based on simulation for fingerprint at low tau
+    m.params['dec_pulse_multiple'] = 4#lowest multiple of 4 pulses
+
+
+    m.autoconfig()
+    funcs.finish(m, upload =True, debug=False)
+    print m.params['sweep_pts'] 
+
+if __name__ == '__main__':
+    Carbon_Ramsey(SAMPLE)
+
+# if __name__ == '__main__':
+#     tau_list = np.linspace(6.522e-6-20e-9,6.522e-6+20e-9,21)
+#     for tau in tau_list:
+#         print tau 
+#         Carbon_Ramsey(SAMPLE+str(tau),tau, N=16)
+
+#         print 'press q now to cleanly exit this measurement loop'
+#         qt.msleep(5)
+#         if (msvcrt.kbhit() and (msvcrt.getch() == 'q')):
+#             break
+
---
+++ scripts/QEC/carbon_ramseys/long_carbon_ramsey_script.py
@@ -0,0 +1,58 @@
+"""
+Script for a carbon ramsey sequence
+"""
+import numpy as np
+import qt
+
+#reload all parameters and modules
+execfile(qt.reload_current_setup)
+import measurement.lib.measurement2.adwin_ssro.dynamicaldecoupling as DD
+import measurement.scripts.mbi.mbi_funcs as funcs
+
+reload(DD)
+
+SAMPLE = qt.exp_params['samples']['current']
+SAMPLE_CFG = qt.exp_params['protocols']['current']
+
+def Long_Carbon_Ramsey(name,tau = None,Addressed_Carbon = 1):
+
+    m = DD.LongNuclearRamsey(name)
+    funcs.prepare(m)
+
+    '''set experimental parameters'''
+    m.params['reps_per_ROsequence'] = 500 #Repetitions of each data point
+    m.params['Ren_Decoupling_scheme'] = 'repeating_T_elt'
+    m.params['DD_wait_scheme'] = 'auto'#XY8'
+
+    ### Sweep parameters
+
+    m.params['N_list'] = range(8,180,8)# np.ones(len(m.params['Phases_of_Ren_B']))*4 #
+    m.params['Phases_of_Ren_B'] = np.ones(len(m.params['N_list']))*0 
+
+    # m.params['N_list'] = np.ones(21)*4#
+    # m.params['Phases_of_Ren_B'] = np.linspace(0,360*2,21)  
+
+ 
+    m.params['C'+str(Addressed_Carbon)+'_freq'] = ( 
+        m.params['C'+str(Addressed_Carbon)+'_freq']+.5e3) # Overwrites the msmst params. Usefull to calibrate and find the correct freq 
+    
+    tau_larmor = m.get_tau_larmor()
+    m.params['tau_list']           = np.ones(len(m.params['N_list']) )*tau_larmor*8
+    m.params['Addressed_Carbon'] = Addressed_Carbon 
+ 
+
+    m.params['pts']              = len(m.params['Phases_of_Ren_B'])
+    # m.params['sweep_pts']        =m.params['Phases_of_Ren_B']
+    # m.params['sweep_name']       = 'Phase'
+    m.params['sweep_pts']      = np.ones(len(m.params['N_list'])) #NB! This value is overwritten in the measurement class 
+                                                   # when the sweep name is 'Free Evolution Time (s)' 
+    m.params['sweep_name'] = 'Free Evolution time (s)' 
+
+
+
+    m.autoconfig()
+    funcs.finish(m, upload =True, debug=False)
+
+if __name__ == '__main__':
+    Long_Carbon_Ramsey(SAMPLE,Addressed_Carbon = 4)
+
