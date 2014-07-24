efc6635eab28d9e2d07d6be73f2ff748132b942c
Team Diamond <cryolt2@gmail.com>
2014-07-24 16:45:11 +0200

    bell bash

--- scripts/bell/joint_params.py
+++ scripts/bell/joint_params.py
@@ -1,16 +1,16 @@
 joint_params = {}
 
 ### default process settings
-joint_params['RO_during_LDE'] = 0
+joint_params['RO_during_LDE'] = 1
 
 joint_params['opt_pi_pulses'] = 2
 joint_params['LDE_attempts_before_CR'] = 250 # 1000 for tpqi seems ok
 joint_params['initial_delay']           = 10e-9 ## 2014-06-07 initial delay used to be a joint param. i made it setup specific, to overlap the pulses
 joint_params['opt_pulse_separation']    = 600e-9
 
-joint_params['RND_during_LDE'] = 0 
-joint_params['do_echo'] = 0
-joint_params['do_final_MW_rotation'] = 0
+joint_params['RND_during_LDE'] = 1 
+joint_params['do_echo'] = 1
+joint_params['do_final_MW_rotation'] = 1
 joint_params['wait_for_1st_revival'] = 0
 joint_params['DD_number_pi_pulses'] = 2 # the maximum number of pi pulses is 3 !!!
 
