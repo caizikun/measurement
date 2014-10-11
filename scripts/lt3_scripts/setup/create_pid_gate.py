05f630ea92fb632c982c4b5dd7fedaa30d765c03
Team diamond lt3 <cryolt2@gmail.com>
2014-10-11 16:50:55 +0200

    lt3_scripts/setup/create_pid_gate.py abash

--- scripts/lt3_scripts/setup/create_pid_gate.py
+++ scripts/lt3_scripts/setup/create_pid_gate.py
@@ -1,25 +1,24 @@
 
-if True:
-    _setctrl_gate = lambda x: qt.instruments['ivvi'].set_dac3(x)
+#Stabilize the gate using information from the gate modulation:
+if False:
+    _setctrl_gate = lambda x: qt.instruments['ivvi'].set_dac4(x)
     _getval_gate = lambda: qt.instruments['physical_adwin'].Get_FPar(79)
-    _getctrl_gate=  lambda: qt.instruments['ivvi'].get_dac3()
+    _getctrl_gate=  lambda: qt.instruments['ivvi'].get_dac4()
     pidgate = qt.instruments.create('pidgate', 'pid_controller_v4', 
             set_ctrl_func=_setctrl_gate , get_val_func=_getval_gate , get_ctrl_func=_getctrl_gate, 
             ctrl_minval=-1800, ctrl_maxval=-800)
 
-
-#if True:
-#    _setctrl_gate = lambda x: qt.instruments['ivvi'].set_dac4(x)
-#    _getval_gate = lambda: qt.instruments['physical_adwin'].Get_FPar(79)
-#    _getctrl_gate=  lambda: qt.instruments['ivvi'].get_dac4()
-#    pidgate = qt.instruments.create('pidgate', 'pid_controller_v4', 
-#            set_ctrl_func=_setctrl_gate , get_val_func=_getval_gate , get_ctrl_func=_getctrl_gate, 
-#            ctrl_minval=-400, ctrl_maxval=400)
-
-
-
-
+#Stabilize the gate using information from the gate modulation:
 if True:
+    _setctrl_gate = lambda x: qt.instruments['ivvi'].set_dac4(x)
+    _getval_gate = lambda: qt.instruments['physical_adwin'].Get_FPar(79)
+    _getctrl_gate=  lambda: qt.instruments['ivvi'].get_dac4()
+    pidgate = qt.instruments.create('pidgate', 'pid_controller_v4', 
+            set_ctrl_func=_setctrl_gate , get_val_func=_getval_gate , get_ctrl_func=_getctrl_gate, 
+            ctrl_minval=0, ctrl_maxval=400)
+
+#Stabilize the Yellow frequency using information from the yellow AOM modulation:
+if False:
     _setctrl_yellow_freq = lambda x: qt.instruments['physical_adwin'].Set_FPar(52,x)
     _getval_yellow_freq = lambda: qt.instruments['physical_adwin'].Get_FPar(78)
     _getctrl_yellow_freq=  lambda: qt.instruments['physical_adwin'].Get_FPar(42)
@@ -27,7 +26,8 @@ if True:
             set_ctrl_func=_setctrl_yellow_freq , get_val_func=_getval_yellow_freq , get_ctrl_func=_getctrl_yellow_freq, 
             ctrl_minval=0., ctrl_maxval=35.)
 
-if True:
+#Stabilize the Newfocus frequency using information from the taper & yellow frequencies (measured on local WM):
+if False:
     set_eprime_func = lambda x: qt.instruments['physical_adwin'].Set_FPar(51,x)
     get_eprime_func = lambda: qt.instruments['physical_adwin'].Get_FPar(41)
     get_E_y_func = lambda: qt.instruments['physical_adwin'].Get_FPar(43)
@@ -40,6 +40,8 @@ if True:
         get_Y_func=get_Y_func,
         set_strain_splitting_func=set_strain_splitting_func)
 
+
+#Stabilize the Taper frequency using information from the gate modulation:
 if False:
     _setctrl_taper_freq = lambda x: qt.instruments['physical_adwin'].Set_FPar(53,x)
     _getval_taper_freq = lambda: qt.instruments['physical_adwin'].Get_FPar(79)
