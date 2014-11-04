0f4f7b44c06cd5c22125a32a18a7b291b44dee52
Team diamond lt3 <cryolt2@gmail.com>
2014-11-04 17:56:52 +0100

    Bell

--- scripts/bell/bell.py
+++ scripts/bell/bell.py
@@ -5,8 +5,8 @@ import logging
 import measurement.lib.measurement2.measurement as m2
 import measurement.lib.measurement2.pq.pq_measurement as pq
 from measurement.lib.measurement2.adwin_ssro import pulsar_pq
-from measurement.lib.cython.PQ_T2_tools import T2_tools_v2 , T2_tools_bell, T2_tools_bell_LTSetups
-reload(T2_tools_bell)
+from measurement.lib.cython.PQ_T2_tools import T2_tools_v2 , T2_tools_bell_BS, T2_tools_bell_LTSetups
+reload(T2_tools_bell_BS)
 reload(T2_tools_v2)
 reload(T2_tools_bell_LTSetups)
 
@@ -179,13 +179,13 @@ class Bell(pulsar_pq.PQPulsarMeasurement):
                 if qt.current_setup in ('lt4', 'lt3'):
                     hhtime, hhchannel, hhspecial, sync_time, sync_number, \
                         newlength, t_ofl, t_lastsync, last_sync_number = \
-                        T2_tools_bell_LTSetups.Bell_live_filter(_t, _c, _s, self.hist,t_ofl, t_lastsync, last_sync_number,
+                        T2_tools_bell_LTSetups.Bell_live_filter(_t, _c, _s, t_ofl, t_lastsync, last_sync_number,
                                                 MIN_SYNC_BIN, MAX_SYNC_BIN,
                                                 T2_WRAPAROUND,T2_TIMEFACTOR)
                 else:       
                     hhtime, hhchannel, hhspecial, sync_time, self.hist, sync_number, \
                         newlength, t_ofl, t_lastsync, last_sync_number, new_entanglement_markers = \
-                        T2_tools_bell.Bell_live_filter(_t, _c, _s, self.hist, t_ofl, t_lastsync, last_sync_number,
+                        T2_tools_bell_BS.Bell_live_filter(_t, _c, _s, self.hist, t_ofl, t_lastsync, last_sync_number,
                         MIN_SYNC_BIN, MAX_SYNC_BIN, MIN_HIST_SYNC_BIN, MAX_HIST_SYNC_BIN, T2_WRAPAROUND,T2_TIMEFACTOR) 
 
 
