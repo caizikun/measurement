b334c537e280039025300411b3c4296de1273e1e
Team diamond lt3 <cryolt2@gmail.com>
2014-12-04 17:55:07 +0100

    bell scripts

--- scripts/bell/sweep_Bell.py
+++ scripts/bell/sweep_Bell.py
@@ -139,7 +139,7 @@ def tail_sweep(name):
     m=SweepBell('tail_sweep_'+name)
     _setup_params(m, setup = qt.current_setup)
 
-    pts=11
+    pts=15
     m.params['pts']=pts
     m.params['repetitions'] = 500
 
@@ -161,7 +161,7 @@ def tail_sweep(name):
         p_aom= qt.instruments['PulseAOM']
         aom_voltage_sweep = np.zeros(pts)
         max_power_aom=p_aom.voltage_to_power(p_aom.get_V_max())
-        aom_power_sweep=np.linspace(.3,1,pts)*max_power_aom #%power
+        aom_power_sweep=np.linspace(.001,0.95,pts)*max_power_aom #%power
         for i,p in enumerate(aom_power_sweep):
             aom_voltage_sweep[i]= p_aom.power_to_voltage(p)
 
@@ -209,12 +209,13 @@ def OptPulseSep_sweep(name):
     run_sweep(m, th_debug=False, measure_bs=False, upload_only = False)
 
 def echo_sweep(name):
+    print 'setting up the echo sweep'
     m=SweepBell('echo_sweep_'+name)
     _setup_params(m, setup = qt.current_setup)
 
-    pts=7
+    pts=1
     m.params['pts']=pts
-    m.params['repetitions'] = 10000
+    m.params['repetitions'] = 20000
     
     m.joint_params['RND_during_LDE'] = 0
     m.joint_params['RO_during_LDE'] = 0
@@ -292,8 +293,8 @@ def run_sweep(m, th_debug=False, measure_bs=True, upload_only = False):
 
 
 if __name__ == '__main__':
-    OptPulseSep_sweep('Pippin')
-    #tail_sweep('tail_lt3_Pippin') 
+    #OptPulseSep_sweep('Pippin')
+    tail_sweep('tail_lt3_Pippin') 
     #tune('tune_lt3_PippinSil3') 
     #echo_sweep('Pippin_SIL3_1_DD_pi_pulse')
     #rnd_echo_ro('test')
