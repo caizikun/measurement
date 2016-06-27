63fc10aabd95414938e235965a9154570133b84a
Team diamond lt3 <cryolt2@gmail.com>
2016-06-27 18:18:14 +0200

    Attempt to add code so that lt4 can run lt3

--- scripts/Purification/purify.py
+++ scripts/Purification/purify.py
@@ -95,10 +95,22 @@ class purify(PQPurifyMeasurement):
         PQPurifyMeasurement.finish(self)
 
 
-    def run(self, autoconfig=False, setup=False, debug=False, live_filter_on_marker=False):
+    def run(self, autoconfig=False, setup=False, debug=False, live_filter_on_marker=False, control_slave = False):
         if debug:
             self.run_debug()
             return
+
+        # Experimental addition for remote running
+        if (self.current_setup == self.joint_params['master_setup']) and control_slave:
+            qt.instruments['lt3_helper'].set_is_running(False)
+            qt.msleep(0.5)
+            qt.instruments['lt3_helper'].set_measurement_name(name)
+            qt.instruments['lt3_helper'].set_script_path(r'D:/measuring/measurement/scripts/Purification/purify.py')
+            qt.instruments['lt3_helper'].execute_script()
+            qt.instruments['lt4_helper'].set_is_running(True)
+            
+            m.lt3_helper.set_is_running(True)
+            qt.msleep(2)
 
         if autoconfig:
             self.autoconfig()
