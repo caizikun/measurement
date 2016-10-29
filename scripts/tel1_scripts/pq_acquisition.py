5607b84f6284672692ba92236cde27dc37826f46
teamdiamond <cryolt2@gmail.com>
2016-10-29 13:37:37 +0200

    Few Telecom scripts from TEL1 - Anais

--- scripts/tel1_scripts/pq_acquisition.py
+++ scripts/tel1_scripts/pq_acquisition.py
@@ -200,16 +200,54 @@ def run_HH(name) :
     m.params['MAX_DATA_LEN'] =       int(100e6) ## used to be 100e6
     m.params['BINSIZE'] =            8 #2**BINSIZE*BASERESOLUTION 
     m.params['MIN_SYNC_BIN'] =       int(2e6)#1500
-    m.params['MAX_SYNC_BIN'] =       int(3.5e6)#3000
+    m.params['MAX_SYNC_BIN'] =       int(5.5e6)#3000
     m.params['MIN_HIST_SYNC_BIN'] =  int(2e6)#2600
-    m.params['MAX_HIST_SYNC_BIN'] =  int(3.5e6)#2900
+    m.params['MAX_HIST_SYNC_BIN'] =  int(5.5e6)#2900
     m.params['TTTR_RepetitiveReadouts'] =  10 #
     m.params['TTTR_read_count'] =   m.PQ_ins.get_T2_READMAX() #(=131072)
     m.params['measurement_abort_check_interval']    = 2 #sec
     m.params['wait_for_late_data'] = 2 #in units of measurement_abort_check_interval
     
     m.params['use_live_marker_filter'] = False
-    m.params['measurement_time'] = 1000 #1200 #sec
+    m.params['measurement_time'] = 10000 #1200 #sec
+    m.params['count_marker_channel'] = 1
+
+    #m.params['use_live_marker_filter']=False
+
+    m.PQ_ins.start_T2_mode()
+    m.PQ_ins.calibrate()
+    #m.PQ_ins.StartMeas(int(m.params['measurement_time'] * 1e3))
+    #for i in np.arange(30):
+    #    print i
+    #    cur_length, cur_data = m.PQ_ins.get_TTTR_Data(count = int(512*200))
+    #    print cur_length
+    #    qt.msleep(1)
+
+    m.run( )
+    m.finish()
+    #m.PQ_ins.StopMeas()
+
+
+
+def run_for_sweep_tail(name) :   
+
+    m = PQTelecomMeasurement(name)
+
+    #m.PQ_ins == qt.instruments['HH_400']
+
+    m.params['MAX_DATA_LEN'] =       int(100e6) ## used to be 100e6
+    m.params['BINSIZE'] =            8 #2**BINSIZE*BASERESOLUTION 
+    m.params['MIN_SYNC_BIN'] =       int(2.2e6)#1500
+    m.params['MAX_SYNC_BIN'] =       int(3e6)#3000
+    m.params['MIN_HIST_SYNC_BIN'] =  int(2e6)#2600
+    m.params['MAX_HIST_SYNC_BIN'] =  int(3e6)#2900
+    m.params['TTTR_RepetitiveReadouts'] =  10 #
+    m.params['TTTR_read_count'] =   m.PQ_ins.get_T2_READMAX() #(=131072)
+    m.params['measurement_abort_check_interval']    = 2 #sec
+    m.params['wait_for_late_data'] = 2 #in units of measurement_abort_check_interval
+    
+    m.params['use_live_marker_filter'] = False
+    m.params['measurement_time'] = 10000 #1200 #sec
     m.params['count_marker_channel'] = 1
 
     #m.params['use_live_marker_filter']=False
@@ -230,6 +268,6 @@ def run_HH(name) :
 
 
 
-
 if __name__ == '__main__':
-    run_HH('test_remote') 
+    run_HH('HBT_tail') 
+    #run_for_sweep_tail('Tail_red')
