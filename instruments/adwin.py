9641f375fd38a5139dfe3ea64efa27a2bae4abe4
Cristian Bonato <c.bonato@tudelft.nl>
2017-05-02 16:08:31 +0200

    small changes

--- instruments/adwin.py
+++ instruments/adwin.py
@@ -575,6 +575,7 @@ class adwin(Instrument):
             if True, check if linescan is running, and if so, quit right away
         
         """
+
         if abort_if_running and self.is_linescan_running():
             return
                
@@ -595,6 +596,7 @@ class adwin(Instrument):
         p = self.processes['linescan']
         dacs = [ self.dacs[n] for n in dac_names ] 
         
+        self.physical_adwin.Load(os.path.join(self.process_dir, p['file'])) #SvD: added this to prevent error 100
         # set all the required input params for the adwin process
         # see the adwin process for details
         self.physical_adwin.Set_Par(p['par']['set_cnt_dacs'], len(dac_names))
